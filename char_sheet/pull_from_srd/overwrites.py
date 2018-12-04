6ffc3b1bdf1148a5fe50495dd71857dcfb2fb587
Jonathan Roberts <hiafi3@gmail.com>
2018-12-04 02:02:34 -0800

    Last bit of cleanup

--- char_sheet/pull_from_srd/overwrites.py
+++ char_sheet/pull_from_srd/overwrites.py
@@ -1,5 +1,82 @@
 OVERWRITES = {
     "general": {},
+    "barbarian": {
+        "Armored Hulk": {
+            "level": None,
+            "actions": "0",
+            "text": "Some barbarians think armor is too clumsy to actually use. They just haven't seen a warrior in full "
+                    "power armor with a chainsword yet... You are now trained in heavy armor. At level 8 you are now an "
+                    "expert in heavy armor, and at level 16 you are a master in heavy armor.",
+        }
 
+    },
+    "powers": {
+        "Minute Meteors": {
+            "actions": None,
+            "area": "10-foot column",
+            "cost": 1,
+            "crit_failure": "The creature takes double damage.",
+            "crit_success": "The creature takes no damage.",
+            "description": "You can summon a rain of tiny meteorites to fall in a 10-foot column, 30 feet high, with a range of 30 feet. Each target in the area must make a reflex save.",
+            "duration": None,
+            "failure": "The target takes 2d8 points of fire damage",
+            "heighten": "Heightened (+1): Add 1d8 fire damage per level",
+            "level": 1,
+            "material": False,
+            "power": True,
+            "range": None,
+            "reaction": False,
+            "somatic": True,
+            "success": "The creature takes half damage.",
+            "traits": "Evocation",
+            "verbal": True
+        },
+        "Aurora Borealis": {
+            "actions": None,
+            "area": None,
+            "cost": 2,
+            "crit_failure": None,
+            "crit_success": None,
+            "description": "You can create a sheet of cascading colors. This power acts as a wall of fire, but it "
+                           "inflicts cold damage and does not radiate heat. However, one side of the aurora designated "
+                           "by you fascinates creatures within 10 feet, up to a maximum of 2 HD of creatures per "
+                           "sorcerer level. A Will save negates this fascinate effect. You can maintain the wall as "
+                           "long as you spend an action concentrating.",
+            "duration": None,
+            "failure": None,
+            "highten": None,
+            "level": 2,
+            "material": False,
+            "power": False,
+            "range": None,
+            "reaction": False,
+            "somatic": True,
+            "success": False,
+            "traits": "Evocation",
+            "verbal": True
+        },
+        "Breaching the Gulf": {
+            "actions": None,
+            "area": None,
+            "crit_failure": None,
+            "crit_success": None,
+            "description": "You can teleport a single creature within 30 feet into the void of space if it fails "
+                           "a Will save. The target can attempt a new saving throw as a full-round action each "
+                           "round to return. While trapped in the airless void, the target suffers 6d6 points of "
+                           "cold damage per round and must hold its breath or begin to suffocate.",
+            "duration": None,
+            "failure": None,
+            "highten": None,
+            "level": 2,
+            "material": False,
+            "power": False,
+            "range": None,
+            "reaction": False,
+            "somatic": False,
+            "success": False,
+            "traits": "Evocation",
+            "verbal": False
+        },
+    }
 
 }
--- char_sheet/pull_from_srd/pull_from_srd.py
+++ char_sheet/pull_from_srd/pull_from_srd.py
@@ -1,12 +1,15 @@
-import requests
-from lxml import html
-import re
+import collections
 import json
+import re
+
+from bs4 import BeautifulSoup
+import requests
+from unidecode import unidecode
+
 from char_sheet.pull_from_srd.ignores import IGNORES
 from char_sheet.pull_from_srd.overwrites import OVERWRITES
 
-from pyquery import PyQuery as pq
-from unidecode import unidecode
+ability_name_regex_2 = r"(.+) (\d+)[rdstnh][rdstnh]"
 
 
 class Section(object):
@@ -21,8 +24,8 @@ class Section(object):
             inner_html = title.find("span")
             if re.search(r"Feat (\d+)", title.find("span").text):
                 return Feat(title)
-            # elif re.search(r"Feat (\d+)", Section.parse_children_str(inner_html)):
-            #     return Feat(inner_html)
+            if re.search(ability_name_regex_2, title.find("span").text):
+                return Feat(title)
         except Exception:
             pass
         return Section(title)
@@ -30,6 +33,7 @@ class Section(object):
     def find_term(self, regex_str, str_to_search=None):
         if str_to_search is None:
             for c in self.contents:
+                t = self.content_text(c)
                 m = re.search(regex_str, self.content_text(c))
                 if m:
                     return m.group(1)
@@ -44,12 +48,11 @@ class Section(object):
         if self.title is None:
             return "Opening"
         if self.title.find("span").text is None:
-            print(self.title)
             return None
-        return self.title.find("span").text.strip()
+        title_name = unidecode(self.title.find("span").text.strip())
+        return title_name
 
     def add_contents(self, content):
-
         self.contents.append(content)
 
     @property
@@ -61,21 +64,10 @@ class Section(object):
             "text": self.contents_text
         }
 
-    @staticmethod
-    def parse_children_str(child_obj):
-        return str(html.tostring(child_obj))\
-            .replace("<p>", "")\
-            .replace("</p>", "")\
-            .replace("<span>", "")\
-            .replace("</span>", "")
-
     def content_text(self, content):
         if isinstance(content, Section):
             return content.contents_text
-        elif len(content):
-            return self.parse_children_str(content)
-        else:
-            return unidecode(content.text)
+        return unidecode(content.text)
 
     @property
     def contents_text(self):
@@ -83,11 +75,9 @@ class Section(object):
         for c in self.contents:
             if isinstance(c, Section):
                 element_text.append(c.contents_text)
-            if len(c):
-                element_text.append(self.parse_children_str(c))
             else:
-                element_text.append(unidecode(c.text))
-        return " ".join(element_text)
+                element_text.append(c.text)
+        return unidecode(" ".join(element_text))
 
     def __str__(self):
         return "{}: {}".format(self.title_text, self.contents_text)
@@ -100,7 +90,10 @@ class Feat(Section):
 
     @property
     def key_name(self):
-        return re.search("(.+) Feat \d+", self.title_text).group(1)
+        search1 = re.search("(.+) Feat \d+", self.title_text)
+        if search1:
+            return search1.group(1)
+        return re.search(ability_name_regex_2, self.title_text).group(1)
 
     @property
     def required_level(self):
@@ -125,10 +118,46 @@ class Feat(Section):
             "prerequisite": self.prereq,
             "trigger": self.trigger,
             "requirements": self.requirements,
+            "actions": "0",
         }
 
 
 class Spell(Section):
+
+    def find_property_in_contents(self, prop_name):
+        next_prop = False
+        for c in self.contents[2].contents:
+            if next_prop:
+                return c.strip().lstrip()
+            try:
+                if c.text == prop_name:
+                    next_prop = True
+            except Exception:
+                pass
+        return None
+
+    def find_in_contents(self, term, group=None, node=None):
+        if node is None:
+            node = self.contents
+        vals_to_return = []
+        for c in node:
+            try:
+                if c.name == "ul":
+                    ret_value = self.find_in_contents(term, group, c.contents)
+                    if ret_value:
+                        vals_to_return.append(self.find_in_contents(term, group, c.contents))
+                else:
+                    match = re.search(term, c.text)
+                    if match is not None:
+                        if group:
+                            vals_to_return.append(match.group(group))
+                        else:
+                            vals_to_return.append(c.text)
+            except Exception:
+                pass
+        if vals_to_return:
+            return "\n".join(vals_to_return)
+        return None
 
     @property
     def title_text(self):
@@ -136,36 +165,138 @@ class Spell(Section):
 
     @property
     def level(self):
-        return self.find_term(r"Power</a> (\d+)") or self.find_term(r"Spell</a> (\d+)")
+        return self.find_term(r"Power (\d+).*") or self.find_term(r"Spell (\d+).*")
+
+    @property
+    def power(self):
+        return self.find_term(r"Power (\d+).*") is not None
+
+    @property
+    def cost(self):
+        return "1"
 
     @property
     def traits(self):
-        return self.find_term(r"<b>Traits</b> (.+)")
+        return self.find_term(r"Traits (.+)")
 
     @property
     def actions(self):
-        return self.find_term(r"<b>Actions</b> (.+?)<br>")
+        return self.find_property_in_contents("Actions")
+
+    @property
+    def reaction(self):
+        if self.actions is None:
+            return False
+        return re.search(r".*Reaction.*", self.actions) is not None
+
+    @property
+    def verbal(self):
+        if self.actions is None:
+            return False
+        return re.search(r".*Verbal.*", self.actions) is not None
+
+    @property
+    def material(self):
+        if self.actions is None:
+            return False
+        return re.search(r".*Material.*", self.actions) is not None
+
+    @property
+    def somatic(self):
+        if self.actions is None:
+            return False
+        return re.search(r".*Somatic.*", self.actions) is not None
 
     @property
     def area(self):
-        return self.find_term(r"<b>Area</b> (.+)<br>")
+        return self.find_property_in_contents("Area")
+
+    @property
+    def range(self):
+        return self.find_property_in_contents("Range")
 
     @property
     def duration(self):
-        return self.find_term(r"<b>Duration</b> (.+)")
+        return self.find_property_in_contents("Duration")
 
     @property
     def description(self):
-        return self.find_term(r"Description (.+)", self.contents_text)
+        desc = []
+        start_capturing = False
+        for c in self.contents:
+            if not start_capturing:
+                if c.text == "Description":
+                    start_capturing = True
+                continue
+            if c.name != "p":
+                continue
+            if re.match(r"^Success (.*)$", c.text):
+                continue
+            if re.match(r"^Failure (.*)$", c.text):
+                continue
+            if re.match(r"^Critical Failure (.*)$", c.text):
+                continue
+            if re.match(r"^Critical Success (.*)$", c.text):
+                continue
+            if re.match(r"^Critical Success (.*)$", c.text):
+                continue
+            if re.match(r"^Heightened (.*)$", c.text):
+                continue
+            desc.append(c.text)
+        return "\n".join(desc)
+
+    @property
+    def heighten(self):
+        heighten_regex = r"(Heightened \(.*\)) (.*)"
+        heighten_text = self.find_in_contents(heighten_regex)
+        if heighten_text is None:
+            return None
+        split_by_newline = heighten_text.split("\n")
+        new_heighten = []
+        for line in split_by_newline:
+            match = re.match(heighten_regex, line)
+            if match:
+                new_heighten.append("**{}**".format(match.group(1)))
+                new_heighten.append(match.group(2))
+
+        return "\n".join(new_heighten)
+
+    @property
+    def crit_success(self):
+        return self.find_in_contents(r"^Critical Success (.*)$", 1)
+
+    @property
+    def success(self):
+        return self.find_in_contents(r"^Success (.*)$", 1)
+
+    @property
+    def failure(self):
+        return self.find_in_contents(r"^Failure (.*)$", 1)
+
+    @property
+    def crit_failure(self):
+        return self.find_in_contents(r"^Critical Failure (.*)$", 1)
 
     def to_dict(self, ignore=None):
         return {
             "level": int(self.level) if self.level else None,
+            "power": self.power,
             "traits": self.traits,
+            "range": self.range,
+            "cost": self.cost,
             "actions": self.actions,
+            "reaction": self.reaction,
+            "verbal": self.verbal,
+            "somatic": self.somatic,
+            "material": self.material,
             "area": self.area,
             "duration": self.duration,
             "description": self.description,
+            "heighten": self.heighten,
+            "crit_success": self.crit_success,
+            "success": self.success,
+            "failure": self.failure,
+            "crit_failure": self.crit_failure,
         }
 
 
@@ -176,14 +307,19 @@ class GenericPage(object):
         self.name = name
 
     @property
+    def display_name(self):
+        return self.name
+
+    @property
     def url(self):
         return "http://pf2playtest.opengamingnetwork.com/"
 
     def get_page_content(self, url=None):
         if url is None:
             url = self.url
-        bulk_html = pq(requests.get(url).text)
-        return bulk_html(".article-content")
+        response = requests.get(url)
+        bulk_html = BeautifulSoup(unidecode(response.text), 'html5lib')
+        return bulk_html.find("div", attrs={"class": "article-content"})
 
     def to_dict(self, ignore=None):
         return {
@@ -199,14 +335,11 @@ class GenericPage(object):
     def parse(self, create_method=Section.create):
         content = self.get_page_content()
         section = Section(None)
-        for child in content.children():
-            if child.tag in ("hr", ):
-                continue
-            if child.tag in ("h3", "h4"):
-                if section:
-                    self.sections[section.title_text] = section
+        for child in content.find_all():
+            if child.name == "h4":
+                self.sections[section.title_text] = section
                 section = create_method(child)
-            elif section:
+            elif child.name == "p":
                 section.add_contents(child)
         self.sections[section.title_text] = section
 
@@ -218,10 +351,8 @@ class RacePage(GenericPage):
         return "http://pf2playtest.opengamingnetwork.com/ancestries/{}/".format(self.name)
 
     def to_dict(self, ignore=None):
-        return_dict = super(RacePage, self).to_dict()
-        section_name = "Overview"
-        if self.name == 'human':
-            section_name = "Human Adventurers"
+        return_dict = super(RacePage, self).to_dict(ignore)
+        section_name = "Opening"
         return_dict["hp"] = int(re.search(r"(<b>)?Hit Points(</b>)? (?P<val>\d+)", self.sections.get(section_name).contents_text).group("val"))
         return_dict["size"] = re.search(r"\| (<strong>)?Size(</strong>)? (?P<val>.+) \|", self.sections.get(section_name).contents_text).group("val")
         return_dict["speed"] = re.search(r"\| (<strong>)?Speed(</strong>)? (?P<val>\d+)", self.sections.get(section_name).contents_text).group("val")
@@ -234,6 +365,12 @@ class ClassPage(GenericPage):
     def url(self):
         return "http://pf2playtest.opengamingnetwork.com/classes/{}/".format(self.name)
 
+    def to_dict(self, ignore=None):
+        return_dict = super(ClassPage, self).to_dict(ignore)
+        section_name = "Opening"
+        return_dict["hp"] = int(re.search(r"(<b>)?Hit Points(</b>)? (?P<val>\d+)", self.sections.get(section_name).contents_text).group("val"))
+        return return_dict
+
 
 class FeatsPage(GenericPage):
 
@@ -245,14 +382,22 @@ class FeatsPage(GenericPage):
 class SpellPage(GenericPage):
 
     @property
+    def display_name(self):
+        return self.name.replace("-", " ").title()
+
+    def to_dict(self, ignore=None):
+        return self.sections.get("spell").to_dict()
+
+    @property
     def url(self):
         return "http://pf2playtest.opengamingnetwork.com/spells/all-spells/{}/{}/".format(self.name[0].lower(), self.name)
 
     def parse(self, create_method=Section.create):
         content = self.get_page_content()
         section = Spell(self.name)
-        for child in content.children():
-            if child.tag in ("hr", ):
+
+        for child in content.find_all(recursive=False):
+            if child.name in ("hr", ):
                 continue
             elif section:
                 section.add_contents(child)
@@ -260,13 +405,17 @@ class SpellPage(GenericPage):
 
     @staticmethod
     def get_all_spell_names():
-        bulk_html = pq(requests.get("http://pf2playtest.opengamingnetwork.com/spells/all-spells/").text)
-        content = bulk_html(".article-content")(".ogn-childpages")
-        return [c.get("href").split("/")[-2] for c in content.find("li > ul > li > a")]
+        url = "http://pf2playtest.opengamingnetwork.com/spells/all-spells/"
+        bulk_html = BeautifulSoup(requests.get(url).text, 'html5lib')
+        content = bulk_html.find("div", attrs={"class": "article-content"})
+        return [c.get("href").split("/")[-2] for c in content.find_all("a") if len(c.get("href").split("/")[-2]) > 1]
 
 
 def write_spells():
-    return {"spells": write_objs(SpellPage, SpellPage.get_all_spell_names(), "spells")}
+    spell_objs = write_objs(SpellPage, SpellPage.get_all_spell_names())
+    spells = {k: v for k, v in spell_objs.items() if not v.get("power")}
+    powers = {k: v for k, v in spell_objs.items() if v.get("power")}
+    return {"spells": spells, "powers": powers}
 
 
 def write_classes():
@@ -295,18 +444,29 @@ def write_races():
         "goblin",
         "halfling",
         "human",
-        "dwarf",
-        "dwarf",
     ]
     return write_objs(RacePage, classes, ["Overview"])
+
+
+def update_dict(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.Mapping):
+            d[k] = update_dict(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
 
 
 def write_gen_feats():
     feats = FeatsPage("")
     feats.parse()
     ret = {"general": feats.to_dict(IGNORES.get("general", set()) | IGNORES.get("ALL"))}
-    ret.update(OVERWRITES.get("general", {}))
+    ret = update_dict(ret, OVERWRITES.get("general", {}))
     return ret
+
+
+def print_percent(val):
+    print("{}%".format(round(val, 2)))
 
 
 def write_objs(page_obj=None, objs_to_get=None, ignore_list=None):
@@ -314,9 +474,9 @@ def write_objs(page_obj=None, objs_to_get=None, ignore_list=None):
     for index, c in enumerate(objs_to_get):
         page = page_obj(c)
         page.parse()
-        write_dict[page.name] = page.to_dict(IGNORES.get(c, set()) | IGNORES.get("ALL"))
-        write_dict[page.name].update(OVERWRITES.get(c, {}))
-        print("{}%".format(index / len(objs_to_get) * 100.0))
+        write_dict[page.display_name] = page.to_dict(IGNORES.get(c, set()) | IGNORES.get("ALL"))
+        write_dict[page.display_name] = update_dict(write_dict[page.display_name], OVERWRITES.get(c, {}))
+        print_percent(index / len(objs_to_get) * 100.0)
     return write_dict
 
 
@@ -328,13 +488,11 @@ def write_to_file(dict_obj, fname):
 
 def main():
     output = {}
-    # output.update(write_races())
+    output.update(write_races())
     output.update(write_classes())
     output.update(write_gen_feats())
-    # output.update(write_spells())
+    output.update(write_spells())
     write_to_file(output, "feats")
-
-
 
 
 if __name__ == "__main__":
